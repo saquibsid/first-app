f2f58c54030f98baab8d9594cdce199757dfb293
saquibsid <saquib155@gmail.com>
2026-02-02 19:53:46 +0530

    setup prettier

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-import concurrently from "concurrently";
+import concurrently from 'concurrently';
 
 concurrently([
-  {
-    command: "bun run dev",
-    name: "server",
-    prefixColor: "cyan",
-    cwd: "packages/server",
-  },
-  {
-    command: "bun run dev",
-    name: "client",
-    prefixColor: "green",
-    cwd: "packages/client",
-  },
+   {
+      command: 'bun run dev',
+      name: 'server',
+      prefixColor: 'cyan',
+      cwd: 'packages/server',
+   },
+   {
+      command: 'bun run dev',
+      name: 'client',
+      prefixColor: 'green',
+      cwd: 'packages/client',
+   },
 ]);
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,12 +1,12 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello, World!" });
+app.get('/api/hello', (req, res) => {
+   res.json({ message: 'Hello, World!' });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+   console.log(`Server is running on port ${PORT}`);
 });
