e7136ad994aaa12d83f22e5b6b79d46e10e630d3
saquibsid <saquib155@gmail.com>
2026-02-02 20:09:28 +0530

    husky setup

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
    return (
       <div className="pl-4">
          <p className="font-bold">{message}</p>
-         <Button>Click Me</Button>
+         <Button>Click Me!</Button>
       </div>
    );
 }
